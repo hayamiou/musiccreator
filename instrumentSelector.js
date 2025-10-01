64b1e7ef8d2a64b603617ceff9671d62d1ecb401
sacha <sacha.alt@proton.me>
2025-10-01 16:57:19 +0200

    update de 17h

--- instrumentSelector.js
+++ instrumentSelector.js
@@ -1,44 +1,50 @@
 //sons
 const sons = {
-  "piano": "https://tonejs.github.io/audio/berklee/gong_1.mp3",
-  "guitare": "https://tonejs.github.io/audio/berklee/Bang_Tin_1.mp3",
-  "violon": "https://tonejs.github.io/audio/berklee/violin_bounce1.mp3"
+    "violon": "https://raw.githubusercontent.com/hayamiou/musiccreator/main/assets/violin/violin_A4.mp3",
+    "piano": "https://raw.githubusercontent.com/hayamiou/musiccreator/main/assets/piano/piano_A4.mp3",
+    "guitare": "https://raw.githubusercontent.com/hayamiou/musiccreator/main/assets/guitar/guitar_A4.mp3"
 };
+
+
+const notes = ["A4", "B4", "C4", "D4"];
+
+notes.forEach((note, index) => {
+    const btn = document.getElementById(`playBtn${index}`);
+    btn.addEventListener('click', async () => {
+        pitchShift.pitch = index
+        player.start();
+        console.log(`Note ${note} jouée !`);
+    });
+});
 
 // Fonction pour changer l'instrument sélectionné :
 // Met à jour le texte de sélection et charge le son correspondant dans le player
 function changerInstrument(instrument) {
     selection.textContent = "Sélectionné : " + instrument;
     player.load(sons[instrument])
+    console.log("Instrument changé pour :", instrument);
 }
 
 //initialiser le player
 const player = new Tone.Player(
     sons["piano"]
 ).toDestination();
+const pitchShift = new Tone.PitchShift().toDestination();
+player.connect(pitchShift);
 
 // Récupérer le bouton Play 
 // TODO : à suppr et remplacer par un vrai piano
-const playBtn = document.getElementById('playBtn');
+const playBtn = document.getElementById('playBtnA4');
 
-// Ajouter l'événement au clic du bouton Play
-playBtn.addEventListener('click', async () => {
-
-    Tone.loaded().then(() => {
-        player.start();
-    });
-
-    console.log('Note G4 jouée !');
-});
 
 // Récupération du texte de sélection d'instrument
 // Exemple : "Sélectionné : guitare"
 const selection = document.getElementById("selection");
 
 // Ajout des écouteurs d'événement
-document.getElementById("instrumentPiano").addEventListener("click", () => changerInstrument("piano"));
-document.getElementById("instrumentViolon").addEventListener("click", () => changerInstrument("violon"));
-document.getElementById("instrumentGuitare").addEventListener("click", () => changerInstrument("guitare"));
+document.getElementById("pianoBtn").addEventListener("click", () => changerInstrument("piano"));
+document.getElementById("violonBtn").addEventListener("click", () => changerInstrument("violon"));
+document.getElementById("guitarBtn").addEventListener("click", () => changerInstrument("guitare"));
 
 // Initialisation avec l'instrument par défaut
 changerInstrument("Piano")
